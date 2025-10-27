8fed13dd0383b4c2aaa1513e564102a905087310
Théo Delaunay <156187315+theodel26@users.noreply.github.com>
2025-10-27 14:49:22 +0100

    Update start-pandora.sh

--- start-pandora.sh
+++ start-pandora.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# VÉRIFIEZ ET DÉFINISSEZ LE NOM DE L'UTILISATEUR ICI
-PANDORA_USER="user"
-
 # Déplacez-vous dans le répertoire de l'application
-cd /home/$PANDORA_USER/pandora-box
+cd /home/pandora/pandora-box
 
 # 1. Lancer PandoraBox en arrière-plan
-/home/$PANDORA_USER/.local/bin/python3 /home/$PANDORA_USER/pandora-box/pandora-box.py &
+/home/pandora/.local/bin/python3 /home/pandora/pandora-box/pandora-box.py &
 PANDORA_PID=$!
 
 # 2. Lancer le script de surveillance en arrière-plan
 # Il surveillera et corrigera le plein écran en continu
-/home/$PANDORA_USER/pandora-box/fullscreen_watcher.sh &
+/home/pandora/pandora-box/fullscreen_watcher.sh &
 WATCHER_PID=$!
 
 # 3. Attendre que PandoraBox se termine.
